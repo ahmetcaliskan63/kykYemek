4476a3f455d210cf57fb63cf19137bd043b90f1c
ahmetcaliskan63 <ahmetabclife@gmail.com>
2024-11-01 03:50:10 +0300

    dsds

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Liste from "./compenents/liste/Liste";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. hjghghjg jhhjhjjhh
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Liste />
     </div>
   );
 }
---
+++ src/compenents/liste/Liste.jsx
@@ -0,0 +1,190 @@
+import React, { useState } from "react";
+import FreeBreakfastIcon from "@mui/icons-material/FreeBreakfast";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import "./liste.css";
+
+export default function Liste() {
+  const [currentDay, setCurrentDay] = useState(2);
+  const [isDarkMode, setIsDarkMode] = useState(false); // Tema durumu
+
+  const mealData = [
+    {
+      date: "27 Eylül 2024",
+      breakfast: ["Peynir", "Zeytin", "Domates", "Bal", "Ekmek", "Çay"],
+      dinner: [
+        "Mercimek Çorbası",
+        "Tavuk Sote",
+        "Pirinç Pilavı",
+        "Yoğurt",
+        "Çeyrek Ekmek",
+        "500 ml Su",
+      ],
+    },
+    {
+      date: "28 Eylül 2024",
+      breakfast: ["Yumurta", "Zeytin", "Salatalık", "Bal", "Ekmek", "Çay"],
+      dinner: [
+        "Tarhana Çorbası",
+        "Karnıyarık",
+        "Bulgur Pilavı",
+        "Ayran",
+        "Çeyrek Ekmek",
+        "500 ml Su",
+      ],
+    },
+    {
+      date: "29 Eylül 2024",
+      breakfast: ["Peynir", "Zeytin", "Domates", "Bal", "Ekmek", "Çay"],
+      dinner: [
+        "Şehriye / Yayla Çorbası",
+        "Orman Kebabı / Taze Fasülye",
+        "Arpa Şehriye Pilavı",
+        "Yoğurt",
+        "Çeyrek Ekmek",
+        "500 ml Su",
+      ],
+    },
+    {
+      date: "30 Eylül 2024",
+      breakfast: ["Kaşar Peyniri", "Zeytin", "Domates", "Bal", "Ekmek", "Çay"],
+      dinner: [
+        "Domates Çorbası",
+        "Köfte",
+        "Pirinç Pilavı",
+        "Yoğurt",
+        "Çeyrek Ekmek",
+        "500 ml Su",
+      ],
+    },
+    {
+      date: "1 Ekim 2024",
+      breakfast: ["Peynir", "Zeytin", "Salatalık", "Bal", "Ekmek", "Çay"],
+      dinner: [
+        "Yayla Çorbası",
+        "Etli Nohut",
+        "Bulgur Pilavı",
+        "Ayran",
+        "Çeyrek Ekmek",
+        "500 ml Su",
+      ],
+    },
+    {
+      date: "2 Ekim 2024",
+      breakfast: ["Peynir", "Zeytin", "Salatalık", "Bal", "Ekmek", "Çay"],
+      dinner: [
+        "Yayla Çorbası",
+        "Etli Nohut",
+        "Bulgur Pilavı",
+        "Ayran",
+        "Çeyrek Ekmek",
+        "500 ml Su",
+      ],
+    },
+    {
+      date: "3 Ekim 2024",
+      breakfast: ["Peynir", "Zeytin", "Salatalık", "Bal", "Ekmek", "Çay"],
+      dinner: [
+        "Yayla Çorbası",
+        "Etli Nohut",
+        "Bulgur Pilavı",
+        "Ayran",
+        "Çeyrek Ekmek",
+        "500 ml Su",
+      ],
+    },
+    {
+      date: "4 Ekim 2024",
+      breakfast: ["Peynir", "Zeytin", "Salatalık", "Bal", "Ekmek", "Çay"],
+      dinner: [
+        "Yayla Çorbası",
+        "Etli Nohut",
+        "Bulgur Pilavı",
+        "Ayran",
+        "Çeyrek Ekmek",
+        "500 ml Su",
+      ],
+    },
+  ];
+
+  const handlePrevDay = () => {
+    if (currentDay > 0) {
+      setCurrentDay((prevDay) => prevDay - 1);
+    }
+  };
+
+  const handleNextDay = () => {
+    if (currentDay < mealData.length - 1) {
+      setCurrentDay((prevDay) => prevDay + 1);
+    }
+  };
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const { date, breakfast, dinner } = mealData[currentDay];
+
+  return (
+    <div className={`list ${isDarkMode ? "dark" : "light"}`}>
+      {" "}
+      {/* Tema sınıfı */}
+      <div className="mode-toggle" onClick={toggleDarkMode}>
+        {isDarkMode ? <LightModeIcon /> : <DarkModeIcon />}{" "}
+        {/* Işık / Karanlık modu ikonu */}
+      </div>
+      <button
+        onClick={handlePrevDay}
+        className="nav-button left"
+        disabled={currentDay === 0}
+      >
+        ◀
+      </button>
+      <div className="card">
+        <div className="listH1">
+          <h1>{date}</h1>
+        </div>
+        <div className="meal-section">
+          <h2>
+            <FreeBreakfastIcon />
+            Kahvaltı
+          </h2>
+          <ul className="meal-list">
+            {breakfast.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="meal-section">
+          <h2>
+            <RestaurantIcon />
+            Akşam Yemeği
+          </h2>
+          <ul className="meal-list">
+            {dinner.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <button
+        onClick={handleNextDay}
+        className="nav-button right"
+        disabled={currentDay === mealData.length - 1}
+      >
+        ▶
+      </button>
+      <p style={{ textAlign: "center", marginTop: "20px" }}>
+        <a
+          href="https://www.linkedin.com/in/ahmetcaliskann/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="developer-link"
+        >
+          Geliştirici: Ahmet ÇALIŞKAN
+        </a>
+      </p>
+    </div>
+  );
+}
